8976e4b33626b01d75273473b356cfbfe341304f
Liutauras <leq382121@gmail.com>
2022-06-22 22:45:43 +0300

    POT-2 - added user menu in main navigation

--- src/Components/Global/UserSideMenu/UserMenu/UserMenu.tsx
+++ src/Components/Global/UserSideMenu/UserMenu/UserMenu.tsx
@@ -3,62 +3,72 @@ import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
 import { UserInfoContext } from 'src/state/UserInfoContextProvider';
-import { LinkList } from 'src/types/links';
 
-import closeIcon from '../../../Assets/images/close.svg';
 import './UserMenu.scss';
 
 export const UserMenu = () => {
-  const [menuActive, setMenuActive] = useState(false)
+  const [menuActive, setMenuActive] = useState(false);
   const {userInfo, isLoggedIn} = useContext(UserInfoContext);
 
   return (
-    <div className="menuOpen">
-
-      <div className="menuOpen__top">
-        <img src={userInfo.img} alt="userimg" />
+    <div className="UserMenu">
+      <div className="UserMenu__top">
+        <div className="UserMenu__userimg">
+          <img src={userInfo.img} alt="userimg" />
+        </div>
 
         <div
-          onClick={() => setMenuActive(false)}
-          className={classNames('SideMenu__overlay', {'SideMenu__overlay--active': menuActive})}
+          onClick={() => setMenuActive((boolean) => !boolean)}
+          className={classNames('UserMenu__toggle', {'UserMenu__toggle--active': menuActive})}
         >
-          Open
+          Me
         </div>
       </div>
 
-      <div className="menuOpen__bottom">
-        <ul className='menuOpen__list'>
-          <li className='menuOpen__link'><Link to={'#'}>
-            <img src={''} alt="user" />
-            Account
-            </Link>
-          </li>
+      {menuActive && 
+        <div className="UserMenu__bottom">
+          <ul className='UserMenu__list'>
+            <li className='UserMenu__link'>
+              <Link to="#">
+                <img src={''} alt="user" />
+                Account
+              </Link>
+            </li>
 
-          <li className='menuOpen__link'><Link to={'#'}>
-            <img src={''} alt="bell" />
-            Notifications</Link>
-          </li>
+            <li className='UserMenu__link'>
+              <Link to={'#'}>
+                <img src={''} alt="bell" />
+                Notifications
+              </Link>
+            </li>
 
-          <li className='menuOpen__link'><Link to={'#'}>
-            <img src={''} alt="dimensions" />
-            Terms {'&'} Conditions</Link>
-          </li>
+            <li className='UserMenu__link'>
+              <Link to={'#'}>
+                <img src={''} alt="dimensions" />
+                Terms {'&'} Conditions
+              </Link>
+            </li>
 
-          <li className='menuOpen__link'><Link to={'#'}>
-            <img src={''} alt="shield" />
-            Privacy policy</Link>
-          </li>
+            <li className='UserMenu__link'>
+              <Link to={'#'}>
+                <img src={''} alt="shield" />
+                Privacy policy
+              </Link>
+            </li>
 
-          <li className='menuOpen__link'><Link to={'#'}>
-            <img src={''} alt="card" />
-            Billing</Link>
-          </li>
-        </ul>
+            <li className='UserMenu__link'>
+              <Link to={'#'}>
+                <img src={''} alt="card" />
+                Billing
+              </Link>
+            </li>
+          </ul>
 
-        <button className='menuOpen__logout'>
-          Log out
-        </button>
-      </div>
+          <button className='UserMenu__logout'>
+            Log out
+          </button>
+        </div>
+        }
     </div>
   );
 };
