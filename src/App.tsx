ad1fed426e7541e99525a557a2ef053e8c97cbbe
ieva.kaleginiene <ieva.kaleginiene@gmail.com>
2022-09-26 22:54:19 +0300

    secret key

--- src/App.tsx
+++ src/App.tsx
@@ -48,6 +48,8 @@ import { Elements } from '@stripe/react-stripe-js';
 import { fetchTotalSentiment } from './state/reduxstate/projects/thunks';
 import { Modals } from './modals';
 
+// const secretKey = process.env.REACT_APP_STRIPE_PUBISHABLE_KEY;
+
 const secretKey =
   'pk_test_51LSQG2LPHXTxUZlWyvAfxX92AV2docuxwV92qiDuFIP5lzErCWGdxFmvUIXjHmPBfonOTNqR3c3F0pJMobFmzfBN00jIXrnBDk';
 
