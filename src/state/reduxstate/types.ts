7c9283a0f2b9bdd049e9e3399bbcae16a7429c3f
ieva.kaleginiene <ieva.kaleginiene@gmail.com>
2022-10-07 19:19:23 +0300

    take api regarding to environment

--- src/state/reduxstate/types.ts
+++ src/state/reduxstate/types.ts
@@ -5,6 +5,9 @@ export type AppState = ReturnType<typeof rootReducer>;
 ///TODO: Add these to .env file, discuss it with team
 export const api = 'https://api.potato.to';
 
-export const apiv1 = 'http://188.166.2.245/api/v1';
+export const apiv1 =
+  process.env.REACT_APP_ENV === 'development'
+    ? 'http://188.166.2.245/api/v1'
+    : 'https://api.potato.to/api/v1';
 
 export const apiv2 = 'https://api.potato.to/api/v1';
